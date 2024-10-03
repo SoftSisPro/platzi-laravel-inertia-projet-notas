fb92d2b4d6d8341849308ec7133958daf6d4d56f
SoftSisPro <softsispro@gmail.com>
2024-10-02 19:50:35 -0500

    Configuración Inicial0

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Note;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -17,7 +18,10 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'i@admin.com',
+            'password' => bcrypt('123456')
         ]);
+
+        Note::factory(30)->create();
     }
 }
