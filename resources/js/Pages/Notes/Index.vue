e1b4d5e7d2942de341958e09173d89babf5ba463
SoftSisPro <softsispro@gmail.com>
2024-10-02 21:12:16 -0500

    Listamos las notas y agregamos links

--- resources/js/Pages/Notes/Index.vue
+++ resources/js/Pages/Notes/Index.vue
@@ -1,5 +1,9 @@
 <script setup>
-import AppLayout from '@/Layouts/AppLayout.vue';
+    import AppLayout from '@/Layouts/AppLayout.vue';
+    import { Link } from '@inertiajs/vue3';
+    defineProps({
+        notes: Array,
+    })
 </script>
 
 <template>
@@ -18,6 +22,27 @@ import AppLayout from '@/Layouts/AppLayout.vue';
                         <div class="px-4 sm:px-0">
                             <h3 class="text-lg text-gray-900">Listado de Notas</h3>
                             <p class="text-sm text-gray-600">Toma el registro correcto y ejecta cualquier funcion (ver, editar o eliminar)</p>
+                        </div>
+                    </div>
+                    <div class="md:col-span-2 mt-5 md:mt-0">
+                        <div class="shadow bg-white md:rounded-md p-4">
+                            <table>
+                                <tr v-for="(note, i) in notes" :key="i">
+                                    <td class="border px-4 py-2">
+                                        {{ note.excerpt }}
+                                    </td>
+                                    <td class="px-4 py-2">
+                                        <Link :href="route('notes.show', note.id)">
+                                            Ver
+                                        </Link>
+                                    </td>
+                                    <td class="px-4 py-2">
+                                        <Link :href="route('notes.edit', note.id)">
+                                            Editar
+                                        </Link>
+                                    </td>
+                                </tr>
+                            </table>
                         </div>
                     </div>
                 </div>
