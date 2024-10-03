0262638dfa282a6befa9d414e3fe09ea4adbd0b9
SoftSisPro <softsispro@gmail.com>
2024-10-02 20:48:09 -0500

    Base de vista index

---
+++ resources/js/Pages/Notes/Index.vue
@@ -0,0 +1,27 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+</script>
+
+<template>
+    <AppLayout title="Notas">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Modulo de Notas
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <!-- Contenido -->
+                <div class="md:grid md:grid-cols-3 md:gap-6">
+                    <div class="md:col-span-1">
+                        <div class="px-4 sm:px-0">
+                            <h3 class="text-lg text-gray-900">Listado de Notas</h3>
+                            <p class="text-sm text-gray-600">Toma el registro correcto y ejecta cualquier funcion (ver, editar o eliminar)</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
